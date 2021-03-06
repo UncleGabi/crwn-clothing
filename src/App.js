e6b84d39c8c98928af647ba390afab37f5e725d5
UncleGabi <raboczki.gabor92@gmail.com>
2021-03-06 15:18:25 +0100

    finished integrating firebase into app, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -8,20 +8,47 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      {/* Because by doing this our header is always present and rendered 
+import { auth } from "./firebase/firebase.utils";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+    // this closes the subscription
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        {/* Because by doing this our header is always present and rendered 
           despite whatever react-router-dom Switch component and Route components
           decide to render onto the page  */}
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/sign-in" component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/sign-in" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
